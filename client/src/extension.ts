59c852865bb50f4b1aea98a4d3493a6edfb2848a
alacambre-yseop <alacambre@yseop.com>
2018-05-25 10:13:25 +0200

    release(1.0.0): version 1.0.0 (#9)
    * doc(readme): tell how to install via the CLI
    
    * feat(server): add simple completion support
    
    * chore(readme): split readme and changelogs
    
    * chore(licence): update licence
    
    * chore(package): update ackage.json files
    
    * chore(licence): more licence cleaning
    
    * release(0.0.3): version 0.0.3 (#8)
    
    * release(0.0.3): version 0.0.3
    
    * release(date): set release date
    
    * fix(packaging): correctly package server-side libs
    
    * release(1.0.0): version 1.0.0
    
    * chore(licence): update licences

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,5 +1,5 @@
 /* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Copyright (c) Yseop. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 'use strict';
@@ -10,7 +10,8 @@ import { workspace, ExtensionContext } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient';
 
 export function activate(context: ExtensionContext) {
-
+	console.log("Yseop.vscode-yseopml − Activating extension.");
+	
 	// The server is implemented in node
 	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
 	// The debug options for the server
@@ -36,8 +37,8 @@ export function activate(context: ExtensionContext) {
 	}
 	
 	// Create the language client and start the client.
-	let disposable = new LanguageClient('yseopml', 'Yseop Markup Language language server', serverOptions, clientOptions).start();
-	
+	console.log("Yseop.vscode-yseopml − Starting Language Client");
+	let disposable = new LanguageClient('yseopml', 'Yseop Markup Language language server', serverOptions, clientOptions, true).start();
 	// Push the disposable to the context's subscriptions so that the 
 	// client can be deactivated on extension deactivation
 	context.subscriptions.push(disposable);
