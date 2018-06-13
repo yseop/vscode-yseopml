3d936cbc3e295940915f047c4515292f25d22a1d
Maël Minot <mminot@yseop.com>
2018-06-13 16:21:17 +0200

    feat(scripts): minor but nice improvements

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@
 # Dumb script to package the extension and then to install it into VSCode.
 #####
 
+if ! type npm code &> /dev/null
+then
+    echo "$(basename "$0"): Please make sure “npm” and “code” are installed." >&2
+    exit 1
+fi
+
 npm run package || exit
 
 f=$(
@@ -12,16 +18,20 @@ f=$(
 
 if [ -z "$f" ]
 then
-    echo "Could not find extension file." >&2
-    exit 1
+    echo "$(basename "$0"): Could not find extension file." >&2
+    exit 2
 fi
 
 if ! [ -r "$f" ]
 then
-    echo "Could not read “$f”. Note: Maybe your versions of “sort” or “tail” are too old." >&2
-    exit 2
+    echo "$(basename "$0"): Could not read “$f”. Note: Maybe your versions of “sort” or “tail” are too old." >&2
+    exit 3
 fi
 
 echo "Installing “$f”..."
 
-code --install-extension "$f"
+if code --install-extension "$f"
+then
+    echo "$(basename "$0"): Launching VS Code…"
+    code &
+fi
