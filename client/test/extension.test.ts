82bd96cae9067f9490e39eed4a5185e934ff5beb
Antoine LACAMBRE <alacambre@yseop.com>
2018-06-19 16:20:49 +0200

    tests(client): add more useful tests
    − test extension installation
    − test non existing extension, really doesn't exits
    − test that YML language support added

--- client/test/extension.test.ts
+++ client/test/extension.test.ts
@@ -8,16 +8,31 @@ import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-// import * as vscode from 'vscode';
+import * as vscode from 'vscode';
 // import * as myExtension from '../extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function () {
 
-    // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test("Test that YML extension exists.", function(){
+        assert.equal(true, vscode.extensions.all.some(extension => {
+            return extension.id === "Yseop.vscode-yseopml";
+        }));
     });
+
+    test("Test that not existing extension doesn't exists.", function(){
+        assert.equal(false, vscode.extensions.all.some(extension => {
+            return extension.id === "Yseop.notExistingExtension";
+        }));
+    });
+
+    test("Test extension adds yml support.", function(){
+        vscode.languages.getLanguages().then(function (languages) {
+            assert.notEqual(null, languages);
+            assert.notEqual(0, languages.length);
+            assert.notEqual(-1, languages.indexOf("yml"));
+        });
+    });
+
 
 });
