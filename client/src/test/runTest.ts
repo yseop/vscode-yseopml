8c5c67a2ddd07871c5c2681b3956490fb7199ce7
Antoine LACAMBRE <alacambre@yseop.com>
2019-07-05 15:20:02 +0200

    fix: explain relative path; simplify path

--- client/src/test/runTest.ts
+++ client/src/test/runTest.ts
@@ -5,11 +5,12 @@ async function main() {
     try {
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
+        // we are in {workspaceDir}/client/src/test, so go in {workspaceDir}
         const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
 
         // The path to the extension test runner script
         // Passed to --extensionTestsPath
-        const extensionTestsPath = path.resolve(__dirname, './suite/index');
+        const extensionTestsPath = path.resolve(__dirname, 'suite/index');
 
         // Download VS Code, unzip it and run the integration test
         await runTests({ extensionDevelopmentPath, extensionTestsPath });
