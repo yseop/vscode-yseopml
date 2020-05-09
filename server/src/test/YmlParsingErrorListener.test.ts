f264b08434ede3e1748afc54c2b64c18d62b29df
Unknown <antoinelacambre@gmail.com>
2020-05-09 20:25:24 +0200

    test: yml parsing error listener tests

---
+++ server/src/test/YmlParsingErrorListener.test.ts
@@ -0,0 +1,29 @@
+import { CharStreams, CommonTokenStream } from 'antlr4ts';
+import { Diagnostic } from 'vscode-languageserver';
+
+import { YmlLexer, YmlParser } from '../grammar';
+import { YmlParsingErrorListener } from '../visitors';
+
+function getDiagnostics(text: string): Diagnostic[] {
+    const diagnostics: Diagnostic[] = [];
+    const inputStream = CharStreams.fromString(text, '/path/to/file');
+    const lexer = new YmlLexer(inputStream);
+    const tokenStream = new CommonTokenStream(lexer);
+    const parser = new YmlParser(tokenStream);
+    parser.removeErrorListeners();
+    parser.addErrorListener(new YmlParsingErrorListener(diagnostics));
+
+    parser.kaoFile();
+    return diagnostics;
+}
+
+describe('Parsing Error Listener', () => {
+    test('error listener correctly tracks parsing errors', (done) => {
+        expect(getDiagnostics('complete Object')).not.toHaveLength(0);
+        done();
+    });
+    test('no diagnostic found when no parsing error', (done) => {
+        expect(getDiagnostics('complete Object;')).toHaveLength(0);
+        done();
+    });
+});
